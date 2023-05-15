d7bdd1489888307076117c4208057c43cecd31f3
andmaia <andrewrian198@gmail.com>
2023-05-15 18:38:17 -0300

    Adicionado camada de segurança, e validação de token jwt.

---
+++ src/main/java/br/com/fiap/todotarefs/service/AutenticaçãoService.java
@@ -0,0 +1,18 @@
+package br.com.fiap.todotarefs.service;
+
+import br.com.fiap.todotarefs.models.Usuario;
+import br.com.fiap.todotarefs.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class AutenticaçãoService implements UserDetailsService {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.findByEmail(username);
+    }
+
+}
